49924ff53a835035d9c51b95fd2577c4e928d903
Chris Moran <cmoran1989@hotmail.co.uk>
2024-02-17 22:00:53 +0000

    changed hardcoded url

--- src/socket/Socket.ts
+++ src/socket/Socket.ts
@@ -1,12 +1,11 @@
 import { io } from 'socket.io-client';
 
 const env = process.env.NODE_ENV;
-const PORT = env === 'production' ? 4000 : 4000;
-const URL = env === 'production' ? process.env.REACT_APP_SOCKET_URI : 'http://localhost';
+// const URL = env === 'production' ? process.env.REACT_APP_SOCKET_URI : 'http://localhost';
 const KEY =
   env === 'production' ? process.env.REACT_APP_SOCKET_KEY : process.env.REACT_APP_SOCKET_KEY;
 
-export const socket = io(`${URL}`, {
+export const socket = io('api.tints-and-hints.com', {
   autoConnect: false,
   auth: { token: KEY },
   secure: true,
