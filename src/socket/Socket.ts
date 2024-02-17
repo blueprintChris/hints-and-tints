2f4d8ca74608fdc1e854245c3ca16030d9359630
Chris Moran <cmoran1989@hotmail.co.uk>
2024-02-17 21:43:54 +0000

    updated port to 4000

--- src/socket/Socket.ts
+++ src/socket/Socket.ts
@@ -1,12 +1,12 @@
 import { io } from 'socket.io-client';
 
 const env = process.env.NODE_ENV;
-const PORT = env === 'production' ? 443 : 4000;
+const PORT = env === 'production' ? 4000 : 4000;
 const URL = env === 'production' ? process.env.REACT_APP_SOCKET_URI : 'http://localhost';
 const KEY =
   env === 'production' ? process.env.REACT_APP_SOCKET_KEY : process.env.REACT_APP_SOCKET_KEY;
 
-export const socket = io(`wss://${URL}:${PORT}`, {
+export const socket = io(`${URL}:${PORT}`, {
   autoConnect: false,
   auth: { token: KEY },
   secure: true,
